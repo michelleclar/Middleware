e21637fd08595e2702d171e1c675d017466af2a9
carl <carlmichelle493@gmail.com>
2023-08-15 22:15:51 +0800

    工具类模块构建

---
+++ common/src/main/java/org/carl/common/utils/tools/tool.java
@@ -0,0 +1,61 @@
+package org.carl.common.utils.tools;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Properties;
+
+public class tool {
+
+    /**
+     * get classloader from thread context if no classloader found in thread
+     * context return the classloader which has loaded this class
+     *
+     * @return classloader
+     */
+    public static ClassLoader getCurrentClassLoader(Class clazz) {
+        ClassLoader classLoader = Thread.currentThread()
+                .getContextClassLoader();
+        if (classLoader == null) {
+            classLoader = clazz.getClassLoader();
+        }
+        return classLoader;
+    }
+
+    /**
+     * 从classpath 加载配置信息
+     *
+     * @param configFileName 配置文件名称
+     * @return 配置信息
+     * @throws IOException
+     */
+    public static Properties loadFromClasspath(String configFileName,Class clazz) throws IOException {
+        ClassLoader classLoader = getCurrentClassLoader(clazz);
+        Properties config = new Properties();
+
+        List<URL> properties = new ArrayList<URL>();
+        Enumeration<URL> propertyResources = classLoader
+                .getResources(configFileName);
+        while (propertyResources.hasMoreElements()) {
+            properties.add(propertyResources.nextElement());
+        }
+
+        for (URL url : properties) {
+            InputStream is = null;
+            try {
+                is = url.openStream();
+                config.load(is);
+            } finally {
+                if (is != null) {
+                    is.close();
+                    is = null;
+                }
+            }
+        }
+
+        return config;
+    }
+}
